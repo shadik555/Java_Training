905dd170ca173f05df9fe27dfcfa2f1406c58f91
Pavel <shabunyapavel1996@gmail.com>
2021-03-03 03:36:38 +0300

    HomeWork1

---
+++ src/HomeWork_1/MyFamily.java
@@ -0,0 +1,10 @@
+package HomeWork_1;
+//Вывод всех членов семьи с новой строки.
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("Шитыко Александра Сергеевна");
+        System.out.println("Шадурко Артем Павлович");
+        System.out.println("Шабуня Виктор Францевич");
+        System.out.println("Бондаренко Виктория Евгеньевна");
+    }
+}
